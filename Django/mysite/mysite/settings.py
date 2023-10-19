01e06e714a8ab25b8e4208e7a06af2f0bbc4ffd4
tim2004timi <tim2004timi@mail.ru>
2023-10-20 01:54:32 +0300

    Update 15th Django lesson

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e_&zk*d&^o+=f2$r$&4#sx+^(0(np__ajetz@vxd-z4+!$i$32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
