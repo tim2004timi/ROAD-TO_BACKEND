98e10d2d8092e269c77f1574eb86cf491794f1aa
tim2004timi <tim2004timi@mail.ru>
2023-10-21 18:47:05 +0300

    Update Django 17th lesson

--- Django/mysite/myapp/models.py
+++ Django/mysite/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Friends(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_the_best = models.BooleanField(default=True)
